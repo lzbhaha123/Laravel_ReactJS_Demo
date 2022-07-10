7dee25d0de715fbc24b5ed8990a8539c99867f70
lzbhaha123 <845381951@qq.com>
2022-07-10 15:55:39 +1200

    v1

---
+++ app/Admin/Controllers/PostController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Post;
+use \App\Models\Tag;
+
+class PostController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Post';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Post());
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('Title'));
+        $grid->column('picture', __('Picture'));
+        $grid->column('content', __('Content'));
+        $grid->column('category', __('Category'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Post::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('title', __('Title'));
+        $show->field('picture', __('Picture'));
+        $show->field('content', __('Content'));
+        $show->field('category', __('Category'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Post());
+
+        $form->text('title', __('Title'));
+        $form->image('picture', __('Picture'));
+        $form->textarea('content', __('Content'));
+        $form->number('category', __('Category'));
+        $form->multipleSelect('tags','Tag')->options(Tag::all()->pluck('name','id'));
+        return $form;
+    }
+}
